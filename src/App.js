9a1b0f8581b4382d35a82ba80e9d826317efcaff
mdelvalle-creator <mpia.dv95@gmail.com>
2021-08-17 21:45:33 -0300

    entrega

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import './components/NavBar/NavBar.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
+
 function App() {
   return (
     <div className="App">
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
+import './itemListContainer.css'
 
 const ItemListContainer = ({title}) => {
     return (
-        <div>
+        <div className="item-list-container">
             {title || ''}
+            <ItemCount stock={5} initial={1} onAdd={()=>{}} />
         </div>
     )
 }
